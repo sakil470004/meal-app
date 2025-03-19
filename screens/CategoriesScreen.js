f6c08dbc118c79b5ff5541d733eb29926c957983
Mynul Islam <mynul.sakil@gmail.com>
2025-03-20 01:40:07 +0600

    v-93

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,22 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoriesGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoriesGridTile title={itemData.item.title} color={itemData.item.color} />
+    );
+}
+
+function CategoriesScreen() {
+    return (<FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />);
+}
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
