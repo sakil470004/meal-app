8d414fb339df65a550af85d62d31b444e6aed742
Mynul Islam <mynul.sakil@gmail.com>
2025-03-21 01:43:42 +0600

    v-95

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { View, Text } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "react-native-web";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
